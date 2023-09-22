abb6128ca0ba365986c9b36dbab6d8b501eea06a
ArindaRodrick <arinda.rodrick21@gmail.com>
2023-09-22 07:43:44 +0300

    firstcommit

---
+++ app/Models/shopping-lists.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class shopping-lists extends Model
+{
+    use HasFactory;
+}
